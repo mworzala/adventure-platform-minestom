799ef7eafa92b8ab71b97d0cf8757b472f72e8a5
Matt Worzala <mattheworzala@gmail.com>
2021-01-22 01:48:05 -0500

    use a lazily parsed json object to attempt to avoid stringifying and parsing the chat component twice

--- src/main/java/net/kyori/adventure/platform/minestom/MinestomComponentSerializer.java
+++ src/main/java/net/kyori/adventure/platform/minestom/MinestomComponentSerializer.java
@@ -7,6 +7,7 @@ import net.kyori.adventure.text.serializer.ComponentSerializer;
 import net.kyori.adventure.text.serializer.gson.GsonComponentSerializer;
 import net.minestom.server.chat.JsonMessage;
 import org.checkerframework.checker.nullness.qual.NonNull;
+import org.jetbrains.annotations.NotNull;
 
 import static java.util.Objects.requireNonNull;
 
@@ -27,7 +28,28 @@ public final class MinestomComponentSerializer implements ComponentSerializer<Co
 
     @Override
     public @NonNull JsonMessage serialize(@NonNull Component component) {
-        JsonObject serialized = JsonParser.parseString(GSON_SERIALIZER.serialize(requireNonNull(component, "component"))).getAsJsonObject();
-        return new JsonMessage.RawJsonMessage(serialized);
+        return new LazyParseJsonMessage(GSON_SERIALIZER.serialize(requireNonNull(component, "component")));
+    }
+
+    private static class LazyParseJsonMessage extends JsonMessage {
+        private final String rawJson;
+        private JsonObject json = null;
+
+        public LazyParseJsonMessage(@NotNull String rawJson) {
+            this.rawJson = rawJson;
+        }
+
+        @Override
+        public @NotNull JsonObject getJsonObject() {
+            if (this.json == null)
+                this.json = JsonParser.parseString(this.rawJson).getAsJsonObject();
+            return this.json;
+        }
+
+
+        @Override
+        public @NotNull String toString() {
+            return rawJson;
+        }
     }
 }
